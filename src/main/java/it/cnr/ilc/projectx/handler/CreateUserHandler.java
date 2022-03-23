bf6350859a9d25c52dbe6721cadbde4b3cf8be22
Carmelo Ienco <c.ienco@xeel.tech>
2022-03-23 15:42:32 +0100

    inserimento utenti su keycloak

--- src/main/java/it/cnr/ilc/projectx/handler/CreateUserHandler.java
+++ src/main/java/it/cnr/ilc/projectx/handler/CreateUserHandler.java
@@ -4,6 +4,7 @@ import it.cnr.ilc.projectx.dto.UserDto;
 import it.cnr.ilc.projectx.mediator.RequestHandler;
 import it.cnr.ilc.projectx.model.User;
 import it.cnr.ilc.projectx.request.CreateUser;
+import it.cnr.ilc.projectx.service.KeycloakAdminService;
 import it.cnr.ilc.projectx.service.UserService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -18,8 +19,20 @@ public class CreateUserHandler implements RequestHandler<CreateUser, UserDto> {
     @NonNull
     private final UserService userService;
 
+    @NonNull
+    private final KeycloakAdminService keycloakAdminService;
+
     @Override
     public UserDto handle(CreateUser request) {
-        return userService.save(request.getUser());
+        try {
+            KeycloakAdminService.KeycloakAdminClient keycloakAdminClient = keycloakAdminService.getClient();
+            User userEntity = userService.mapToEntity(request.getUser());
+            keycloakAdminClient.createUser(userEntity, "test");
+
+            return userService.save(request.getUser());
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return null;
+        }
     }
 }
