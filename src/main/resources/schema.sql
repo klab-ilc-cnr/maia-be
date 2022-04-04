ebac6dabd3f3084a266d06598867ea760e07a54b
Carmelo Ienco <c.ienco@xeel.tech>
2022-04-04 09:26:50 +0200

    nuovo schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,36 +27,70 @@ ALTER TABLE user_roles
 --- WORKSPACES
 CREATE TABLE public.workspaces
 (
-    id      serial NOT NULL,
-    user_id bigint NOT NULL,
+    id         serial       NOT NULL,
+    name       varchar(100) NOT NULL,
+    created    timestamp without time zone,
+    created_by bigint,
+    updated    timestamp without time zone,
+    updated_by bigint,
     CONSTRAINT workspaces_pk PRIMARY KEY (id)
 );
 
 --- workspace_items;
 
-CREATE TABLE public.workspace_windows
+CREATE TABLE public.workspaces_tiles
 (
     id           serial NOT NULL,
     workspace_id bigint NOT NULL,
-    window_id    bigint NOT NULL,
+    tile_id      bigint NOT NULL,
     "rank"       bigint NOT NULL DEFAULT 0,
-    CONSTRAINT workspace_windows_pk PRIMARY KEY (id),
-    CONSTRAINT workspace_windows_un UNIQUE (workspace_id, window_id, "rank"),
-    CONSTRAINT workspace_windows_fk FOREIGN KEY (workspace_id) REFERENCES public.workspaces (id)
+    CONSTRAINT workspace_tiles_pk PRIMARY KEY (id),
+    CONSTRAINT workspace_tiles_un UNIQUE (workspace_id, tile_id, "rank"),
+    CONSTRAINT workspace_tiles_fk FOREIGN KEY (workspace_id) REFERENCES public.workspaces (id)
 );
 
 
---- WINDOW
+--- TILE
 
-CREATE TABLE public.window
+CREATE TABLE public.tiles
 (
-    id         serial4 NOT NULL,
-    "type"     varchar NOT NULL,
-    x_position decimal NOT NULL DEFAULT 0,
-    y_position decimal NOT NULL DEFAULT 0,
-    x_size     decimal NOT NULL DEFAULT 10,
-    y_size     decimal NOT NULL DEFAULT 10,
-    CONSTRAINT window_pk PRIMARY KEY (id)
+    id           serial4      NOT NULL,
+    content_type varchar(100) NOT NULL,
+    content_id   bigint       NOT NULL,
+    x_position   decimal      NOT NULL DEFAULT 0,
+    y_position   decimal      NOT NULL DEFAULT 0,
+    x_size       decimal      NOT NULL DEFAULT 10,
+    y_size       decimal      NOT NULL DEFAULT 10,
+    CONSTRAINT tile_pk PRIMARY KEY (id)
+);
+
+
+--- TEXT
+CREATE TABLE public.texts
+(
+    id       serial4 NOT NULL,
+    readonly boolean default false,
+    created    timestamp without time zone,
+    created_by bigint,
+    updated    timestamp without time zone,
+    updated_by bigint,
+    CONSTRAINT texts_pk PRIMARY KEY (id)
+);
+
+--- LAYER
+CREATE TABLE public.layers
+(
+    id      serial4 NOT NULL,
+    text_id bigint  NOT NULL,
+    CONSTRAINT layers_pk PRIMARY KEY (id)
+);
+
+--- ANNOTATION
+CREATE TABLE public.annotations
+(
+    id       serial4 NOT NULL,
+    layer_id bigint  NOT NULL,
+    CONSTRAINT annotations_pk PRIMARY KEY (id)
 );
 
 
