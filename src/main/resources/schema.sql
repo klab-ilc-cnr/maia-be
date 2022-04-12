3b43252f2ac0fba192b1793b886402e8eae6b342
Carmelo Ienco <c.ienco@xeel.tech>
2022-04-12 14:57:37 +0200

    ripristinato schema workspace

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,6 @@ CREATE TABLE public.users
     name       character varying(30) not null,
     surname    character varying(30) not null,
     email      character varying(50) not null,
---     username character varying(50) not null,
     created    timestamp without time zone,
     created_by bigint,
     updated    timestamp without time zone,
@@ -25,22 +24,69 @@ CREATE TABLE public.user_roles
 ALTER TABLE user_roles
     add CONSTRAINT FK_user_roles_user_id FOREIGN KEY (user_id) REFERENCES public.users (id);
 
+--- WORKSPACES
+CREATE TABLE public.workspaces
+(
+    id         serial       NOT NULL,
+    name       varchar(100) NOT NULL,
+    created    timestamp without time zone,
+    created_by bigint,
+    updated    timestamp without time zone,
+    updated_by bigint,
+    CONSTRAINT workspaces_pk PRIMARY KEY (id)
+);
 
--- -- LANGUAGES
--- CREATE TABLE public.users_languages
--- (
---     id      serial                not null,
---     user_id bigint                not null,
---     code    character varying(50) not null,
---     name    character varying(50) not null,
---     CONSTRAINT languages_pkey PRIMARY KEY (id)
--- );
+--- WORKSPACE TILES;
+CREATE TABLE public.workspaces_tiles
+(
+    id           serial NOT NULL,
+    workspace_id bigint NOT NULL,
+    tile_id      bigint NOT NULL,
+    "rank"       bigint NOT NULL DEFAULT 0,
+    CONSTRAINT workspace_tiles_pk PRIMARY KEY (id),
+    CONSTRAINT workspace_tiles_un UNIQUE (workspace_id, tile_id, "rank"),
+    CONSTRAINT workspace_tiles_fk FOREIGN KEY (workspace_id) REFERENCES public.workspaces (id)
+);
 
+--- TILE
+CREATE TABLE public.tiles
+(
+    id           serial4      NOT NULL,
+    content_type varchar(100) NOT NULL,
+    content_id   bigint       NOT NULL,
+    x_position   decimal      NOT NULL DEFAULT 0,
+    y_position   decimal      NOT NULL DEFAULT 0,
+    x_size       decimal      NOT NULL DEFAULT 10,
+    y_size       decimal      NOT NULL DEFAULT 10
+        CONSTRAINT tile_pk PRIMARY KEY (id)
+);
 
--- -- USER_LANGUAGES
--- CREATE TABLE public.users_languages
--- (
---     user_id      bigint not null,
---     language_id bigint not null,
---     CONSTRAINT pkey_user_languages PRIMARY KEY (user_id, languages_id)
--- );
+--- TEXT
+CREATE TABLE public.texts
+(
+    id               serial4 NOT NULL,
+    readonly         boolean default false,
+    created          timestamp without time zone,
+    created_by       bigint,
+    updated          timestamp without time zone,
+    updated_by       bigint,
+    external_node_id bigint  NOT NULL,
+    CONSTRAINT texts_pk PRIMARY KEY (id)
+);
+
+--- LAYER
+CREATE TABLE public.layers
+(
+    id      serial4 NOT NULL,
+    text_id bigint  NOT NULL,
+    CONSTRAINT layers_pk PRIMARY KEY (id)
+);
+
+--- ANNOTATION
+CREATE TABLE public.annotations
+(
+    id               serial4 NOT NULL,
+    layer_id         bigint  NOT NULL,
+    external_node_id bigint  NOT NULL,
+    CONSTRAINT annotations_pk PRIMARY KEY (id)
+);
